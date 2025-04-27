21ece9dafb4ccfcc52eb48a31210cea404f72d1a
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2025-04-27 16:16:05 +0530

    Deploying

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ server.listen(process.env.PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
